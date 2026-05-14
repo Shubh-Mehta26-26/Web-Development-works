1342c426b5d908c95dbb5c0f755b1c99d3ba88ea
Shubh-Mehta26-26 <shubhmehta2604@gmail.com>
2026-05-14 23:08:04 +0530

    React props

---
+++ Reactprops/src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./App.css";
+import Card from "./Components/Card.jsx";
+
+function App() {
+  return (
+    <div>
+      <Card name="Shubham">
+        <h1>hello everyone</h1>
+      </Card>
+    </div>
+  );
+}
+
+export default App;
---
+++ Reactprops/src/Components/Card.jsx
@@ -0,0 +1,11 @@
+
+const Card = (props) => {
+  return (
+    <div>
+      {props.name}
+      {props.children}
+    </div>
+  );
+};
+
+export default Card;
